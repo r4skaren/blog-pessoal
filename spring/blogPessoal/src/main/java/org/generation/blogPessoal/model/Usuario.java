7374d5b4ca16b9fd71ae88b1d57dc8d506adb4ac
r4skaren <karenrodrigues20120@gmail.com>
2021-10-09 01:03:05 -0300

    :rocket: new feature

--- spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,36 +1,68 @@
 package org.generation.blogPessoal.model;
 
+
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name = "usuarios")
+@Table(name = "tb_usuarios")
 public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotBlank(message = "Este espaco nao pode ser nulo e nem conter apenas espacamento!")
-	@Size(min = 5, max = 70, message = "Este espaco nao pode ser nulo")
+	@NotBlank(message = "O atributo nome é obrigatório")
 	private String nome;
 	
-	@NotBlank(message = "Este espaco nao pode ser nulo e nem conter apenas espacamento!")
-	@Size(min = 5, max = 100, message = "Este espaco nao pode ser nulo")
+	@Email(message = "É necessário um email válido.")
+	@NotBlank(message = "O atributo usuário é obrigatório")
 	private String usuario;
 	
-	@NotBlank(message = "Este espaco nao pode ser nulo e nem conter apenas espacamento!")
-	@Size(min = 5, message = "Este espaco nao pode ser nulo")
+	@NotBlank(message = "O atributo senha é obrigatório")
+	@Size(min = 8, message = "O atributo senha deve ter no mínimo 8 caracteres")
 	private String senha;
 	
+	@Column(name = "dt_nascimento")
+	@JsonFormat(pattern="yyyy-MM-dd")
+    @NotBlank(message = "O atributo Data de Nascimento é Obrigatório!")
+    private LocalDate dataNascimento;
+	
+	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List <Postagem> postagem;
+
+	// Primeiro método Construtor
+
+	public Usuario(long id, String nome, String usuario, String senha, LocalDate dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.dataNascimento = dataNascimento;
+	}
+
+	// Segundo método Construtor
+
+	public Usuario() {	}
+
 
 	public long getId() {
-		return id;
+		return this.id;
 	}
 
 	public void setId(long id) {
@@ -38,7 +70,7 @@ public class Usuario {
 	}
 
 	public String getNome() {
-		return nome;
+		return this.nome;
 	}
 
 	public void setNome(String nome) {
@@ -46,7 +78,7 @@ public class Usuario {
 	}
 
 	public String getUsuario() {
-		return usuario;
+		return this.usuario;
 	}
 
 	public void setUsuario(String usuario) {
@@ -54,10 +86,28 @@ public class Usuario {
 	}
 
 	public String getSenha() {
-		return senha;
+		return this.senha;
 	}
 
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public LocalDate getDataNascimento() {
+		return this.dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<Postagem> getPostagem() {
+		return this.postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
 }
+
